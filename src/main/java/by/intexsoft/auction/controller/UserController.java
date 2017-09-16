35bee50a4b40dd367937abbac9621568136667f0
Sergey <fospos@gmail.com>
2017-09-17 01:24:56 +0300

    add login, registration controllers

---
+++ src/main/java/by/intexsoft/auction/controller/UserController.java
@@ -0,0 +1,35 @@
+package by.intexsoft.auction.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import by.intexsoft.auction.model.User;
+import by.intexsoft.auction.service.UserService;
+
+@RestController
+public class UserController {
+
+	//private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	private UserService userService;
+
+	/**
+	 * Registration new {@link User}
+	 */
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ResponseEntity<?> registration(@RequestBody User user) {
+		//LOGGER.info("Start registration user");
+		try {
+			return new ResponseEntity<>(userService.registration(user), HttpStatus.CREATED);
+		} catch (Exception e) {
+			//LOGGER.info("Error in addUser. " + e.getLocalizedMessage());
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
