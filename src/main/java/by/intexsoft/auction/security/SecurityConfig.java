12433b41f4cdda610fdec045129eb12f528753cc
Sergey <fospos@gmail.com>
2017-09-26 18:57:52 +0300

    add password encoder

--- src/main/java/by/intexsoft/auction/security/SecurityConfig.java
+++ src/main/java/by/intexsoft/auction/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package by.intexsoft.auction.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import by.intexsoft.auction.service.AuthenticationService;
@@ -46,5 +49,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 
 }
--- src/main/java/by/intexsoft/auction/service/impl/UserServiceImpl.java
+++ src/main/java/by/intexsoft/auction/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import by.intexsoft.auction.model.Authority;
@@ -19,14 +20,20 @@ import by.intexsoft.auction.service.UserService;
 public class UserServiceImpl extends AbstractServiceEntityImpl<User> implements UserService {
 
 	@Autowired
-	private UserRepository repository;
-
+    private PasswordEncoder passwordEncoder;
+	
 	@Autowired
-	private AuthorityService authorityService;
+	private UserRepository repository;
 	
 	@Override
 	public User getUserByUsername(String username) {
 		return repository.findByUsername(username);
+	}
+	
+	@Override
+	public User save (User user) {
+		if (user.getId() == null) user.password = passwordEncoder.encode(user.password);
+		return repository.save(user);
 	}
 	
 	/*@Override
